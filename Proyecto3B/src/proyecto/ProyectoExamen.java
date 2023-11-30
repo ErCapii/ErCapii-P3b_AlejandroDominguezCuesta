ca27df1cd3e4a2a0039d7169b9215b477f3b8215
ErCapii <151050112+ErCapii@users.noreply.github.com>
2023-11-30 16:18:40 +0100

    Update ProyectoExamen.java

--- Proyecto3B/src/proyecto/ProyectoExamen.java
+++ Proyecto3B/src/proyecto/ProyectoExamen.java
@@ -3,5 +3,6 @@ package proyecto;
 public class ProyectoExamen {
 public static void main(String[] args) {
 	System.out.println("Alejandro Dominguez Cuesta");
+	System.out.println("Estoy llegando al final");
 }
 }
